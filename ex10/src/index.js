09b784ba502b8a9280e1b2d4f3406c65a2b9e0de
Jasmina Secic <jasmina.secic98@outlook.com>
2021-09-16 20:38:38 +0200

    String Variables

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    var myFirstName = "Jasmina";
+    var myLastName = "Šečić";
+    var myHomeTown = "Bugojno";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    }
+}
+console.log(main());
+module.exports=main;
